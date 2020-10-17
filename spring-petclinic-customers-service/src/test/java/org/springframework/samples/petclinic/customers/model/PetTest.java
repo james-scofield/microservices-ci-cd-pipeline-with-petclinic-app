ffd370f304718cd34367a0cf05fca8101e2473da
unknown <james-scofield>
2020-10-17 12:08:04 +0300

    updated petTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
@@ -1,9 +1,11 @@
 package org.springframework.samples.petclinic.customers.model;
+​
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Date;
+​
 import org.junit.jupiter.api.Test;
-
+​
 public class PetTest {
+​
     @Test
     public void testGetName(){
         //Arrange
@@ -13,17 +15,19 @@ public class PetTest {
         //Assert
         assertEquals("Fluffy", pet.getName());
     }
+​
     @Test
     public void testGetOwner(){
         //Arrange
         Pet pet = new Pet();
         Owner owner = new Owner();
-        owner.setFirstName("Call2");
+        owner.setFirstName("Call");
         //Act
         pet.setOwner(owner);
         //Assert
         assertEquals("Call", pet.getOwner().getFirstName());
     }
+
     @Test
     public void testBirthDate(){
         //Arrange
