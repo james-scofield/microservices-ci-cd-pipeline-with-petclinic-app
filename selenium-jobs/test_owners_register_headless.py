572993552ce764d5f9b42a7cc42d53702a3b6cec
unknown <james-scofield>
2020-10-24 19:57:57 +0300

    updated selenium test files

--- selenium-jobs/test_owners_register_headless.py
+++ selenium-jobs/test_owners_register_headless.py
@@ -3,17 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import random
 import os
-
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 chrome_options.add_argument("no-sandbox")
-chrome_options.add_argument("disable-dev-shm-usage")
-
-# Update webdriver instance of chrome-driver with adding chrome options
-driver = webdriver.Chrome(options=chrome_options)
-
-# Connect to the application
+chrome_options.add_argument("disable-dev-shm-usage")# Update webdriver instance of chrome-driver with adding chrome options
+driver = webdriver.Chrome(options=chrome_options)# Connect to the application
 APP_IP = os.environ['MASTER_PUBLIC_IP']
 url = "http://"+APP_IP.strip()+":8080/"
 print(url)
@@ -28,17 +23,22 @@ sleep(2)
 fn_field = driver.find_element_by_name('firstName')
 fn = 'Callahan' + str(random.randint(0, 100))
 fn_field.send_keys(fn)
+sleep(1)
 fn_field = driver.find_element_by_name('lastName')
 fn_field.send_keys('Clarusway')
+sleep(1)
 fn_field = driver.find_element_by_name('address')
 fn_field.send_keys('Ridge Corp. Street')
+sleep(1)
 fn_field = driver.find_element_by_name('city')
 fn_field.send_keys('McLean')
+sleep(1)
 fn_field = driver.find_element_by_name('telephone')
 fn_field.send_keys('+1230576803')
+sleep(1)
 fn_field.send_keys(Keys.ENTER)
-
-# Wait 1 second to get updated Owner List
+sleep(1)
+# Wait 2 second to get updated Owner List
 sleep(2)
 # Verify that new user is added to Owner List
 if fn in driver.page_source:
